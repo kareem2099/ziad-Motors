55c1f1e4690ae2dfe467c1d7081c6faba3a6c977
kareem2099 <kareem20990@yahoo.com>
2024-12-23 19:00:01 +0200

    new changes

--- Marah Motors/services/suggestions/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('inquiryForm').onsubmit = function() {
-    alert("شكرا لتقديم الاستفسارات سوف يقوم فريق المختص بفحص استفسارك بدقة وعمل ما يتم عمله.");
-    window.location.href="/";
-    return false; // Prevent form submission
-};
